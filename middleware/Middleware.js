a6b69e7f0780de6c8e1b2aaa1f648a18fc7a4530
RcpRohit <168912119+RcpRohit@users.noreply.github.com>
2024-11-15 17:35:58 +0530

    Update Middleware.js

--- middleware/Middleware.js
+++ middleware/Middleware.js
@@ -1,4 +1,6 @@
 import UserModel from "../models/User.js"
+import AlumniModel from "../models/reviews.js"
+import StudentModel from "../models/Likes.js"
 
 export const isAdmin = async (req, res, next) => {
   try {
@@ -9,7 +11,6 @@ export const isAdmin = async (req, res, next) => {
     if(userInfo && userInfo.role=="Admin")
     {
         next();
-        //proceed
     }
     else{
         res.status(403).json({
@@ -23,3 +24,53 @@ export const isAdmin = async (req, res, next) => {
     })
   }
 };
+
+export const isAlumni = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    const alumniInfo = await AlumniModel.findOne({ email })
+
+    if(alumniInfo && alumniInfo.role=="Alumni")
+    {
+        next();
+    }
+    else{
+        res.status(403).json({
+            message:"Access Denied, only Admin can access"
+        })
+    }
+
+  } catch (error) {
+    res.status(500).json({
+        message:"Internal server error"
+    })
+  }
+};
+
+export const isStudent = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+
+    const studentiInfo = await StudentModel.findOne({ email })
+
+    if(studentiInfo && studentiInfo.role=="Student")
+    {
+        next();
+    }
+    else{
+        res.status(403).json({
+            message:"Access Denied, only Admin can access"
+        })
+    }
+
+  } 
+  catch(error) 
+  {
+    res.status(500).json({
+        message:"Internal server error"
+    })
+  }
+};
+
+
